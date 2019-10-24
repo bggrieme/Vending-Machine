9f0a3cf7fbabde4ef5675c5f6bfd1bf9633cdaaa
bgrieme <bgrieme@gmail.com>
2019-10-24 03:54:37 -0500

    added getIndex and getName methods

--- src/Currency.cs
+++ src/Currency.cs
@@ -2,9 +2,38 @@
 /*Enum representing values of different US currencies in cents*/
 public enum Currency
 {
-    Penny = 1,
-    Nickel = 5,
-    Dime = 10,
-    Quarter = 25,
-    Dollar = 100
+    //note: this.getIndex() is dependant on the order of this enum and should be updated if this enum is appended
+    PENNY = 1,
+    NICKEL = 5,
+    DIME = 10,
+    QUARTER = 25,
+    DOLLAR = 100
+}
+
+static class CurrencyMethods
+{
+    public static int getIndex(this Currency c)
+    {
+        /*TODO: may be able to write this in such a way that removes magic numbers
+        and thus will not break if the enum is modified.
+        Perhaps by iterating through the enum with a loop, counting and comparing as it goes.*/
+        switch (c)
+        {
+            case Currency.PENNY:
+                return 0;
+            case Currency.NICKEL:
+                return 1;
+            case Currency.DIME:
+                return 2;
+            case Currency.QUARTER:
+                return 3;
+            case Currency.DOLLAR:
+                return 4;
+        }
+    }
+
+    public static string getName(this Currency c)
+    {
+        return Enum.GetName(typeof(Currency), c.getIndex());
+    }
 }

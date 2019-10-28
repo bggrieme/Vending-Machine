b94171fcefaddc489df6a94696d30597b04aefa0
bgrieme <bgrieme@gmail.com>
2019-10-28 02:07:27 -0500

    Added argument to constructor, removed argument from resetBank()

---
+++ src/Till.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic; //dictionary
+
+public class Till
+{
+    private Dictionary<Currency, int> bank, defaultBank; //tracks spent currency
+    public decimal holdings{get; private set;} //stores the balance of currency added but not yet spent
+
+    
+    public Till(Dictionary<Currency, int> defaultBank)
+    {
+        
+    }
+
+    /*Resets the bank to the originally constructed defaultBank*/
+    public void resetBank()
+    {
+
+    }
+
+    /*Inserts currency into the system, adding it to the bank and counting its value into a temporary holding*/
+    public void insertMoney(Currency c)
+    {
+
+    }
+
+    /*Empties the temporary holdings bank, returning each piece of currency*/
+    public Currency returnHolding()
+    {
+        return Currency.PENNY;
+    }
+
+    /*Deducts the value of the given item from the holdings bank*/
+    public void transaction(VendingItem item)
+    {
+
+    }
+
+    /*Checks to make sure the system can make change for any currency given*/
+    private bool canMakeChange()
+    {
+        return true;
+    }
+
+    /*Returns the last currency added*/
+    private Currency rejectInserted()
+    {
+        return Currency.PENNY;
+    }
+}

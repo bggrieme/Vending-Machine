27307e23d545a3157c41b9540e26fd9cb7fc85ae
bgrieme <bgrieme@gmail.com>
2019-10-31 00:24:20 -0500

    Finished constructor and resetBank(). Made canMakeChange() public and changed the argument. Had a better idea on how to go about accepting or rejecting currency input.

--- src/Till.cs
+++ src/Till.cs
@@ -4,11 +4,12 @@ public class Till
 {
     public Dictionary<Currency, int> bank { get; private set; }
     public Dictionary<Currency, int> defaultBank { get; private set; } //the state of the bank when constructed
-    public decimal holdings { get; private set; } //stores the balance of currency added but not yet spent
+    public int holdings { get; private set; } //stores the balance of currency added but not yet spent, in cents
 
 
     public Till(int startingPENNIES, int startingNICKELS, int startingDIMES, int startingQUARTERS, int startingDOLLARS)
     {
+        this.defaultBank = new Dictionary<Currency, int>();
         this.defaultBank.Add(Currency.PENNY, startingPENNIES);
         this.defaultBank.Add(Currency.NICKEL, startingNICKELS);
         this.defaultBank.Add(Currency.DIME, startingDIMES);
@@ -21,7 +22,7 @@ public class Till
     public void resetBank()
     {
         this.bank = this.defaultBank;
-        holdings = 0m;
+        holdings = 0;
     }
 
     /*Checks to see if the till can make change for the given currency. 
@@ -30,27 +31,32 @@ public class Till
     If it can not, returns false.*/
     public bool insertMoney(Currency c, int quantity = 1)
     {
-
-        if (!canMakeChange(c) || quantity <= 0)
+        if (!canMakeChange(c) || quantity <= 0) //if till cannot make change for the currency or the quantity <=0
         {
             return false;
         }
         else
         {
-            
-            return true;
+            this.bank[c] += 1; //increment the bank's count for the given currency (thus accepting it into the bank)
+            this.holdings += (int)c;
+            return insertMoney(c, quantity - 1);
         }
     }
 
     /*Returns currency equivalent to the holdings value using the least change possible*/
     public Dictionary<Currency, int> returnHolding()
     {
+        int tempHoldings = this.holdings;
+        int pennies, nickels, dimes, quarters, dollars;
         Dictionary<Currency, int> refund = new Dictionary<Currency, int>();
+        //TODO
+
+        this.holdings = 0;
         return refund;
     }
 
-    /*Deducts the value from the holdings bank*/
-    public void spend(decimal value)
+    /*Deducts the value from holdings*/
+    public void spend(int val_in_cents)
     {
 
     }
@@ -58,6 +64,7 @@ public class Till
     /*Checks to make sure the system can make change for any currency given*/
     private bool canMakeChange(Currency c)
     {
+        
         return true;
     }
 }

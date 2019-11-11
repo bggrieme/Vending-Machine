3082c731b01ac80aed5aae32054e2b0609af0832
bgrieme <bgrieme@gmail.com>
2019-11-10 23:54:57 -0600

    moved the burden of counting item quantities from VendingItem to Inventory

--- src/VendingItem.cs
+++ src/VendingItem.cs
@@ -3,14 +3,12 @@ using System; //Decimal.Round
 public class VendingItem
 {
     string name{get; set;}
-    int quantity{get; set;}
     decimal price{get; set;} 
 
-    public VendingItem(string givenName, int givenQuantity, decimal givenPrice)
+    public VendingItem(string givenName, decimal givenPrice)
     {
-        name = givenName;
-        quantity = givenQuantity;
-        price = Decimal.Round(givenPrice, 2); //rounds givenPrice to 2 decimal places
+        this.name = givenName;
+        this.price = Decimal.Round(givenPrice, 2); //rounds givenPrice to 2 decimal places
     }
 
 }

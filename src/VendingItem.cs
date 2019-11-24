57b8ca30fb3537fe25f54b8ba68cbce07bc1b343
bgrieme <bgrieme@gmail.com>
2019-11-24 00:15:22 -0600

    Adjusted member access modifiers. name is now public get and set. price is now public get, private set. Added method to set price post-object created.

--- src/VendingItem.cs
+++ src/VendingItem.cs
@@ -2,13 +2,18 @@ using System; //Decimal.Round
 
 public class VendingItem
 {
-    string name{get; set;}
-    decimal price{get; set;} 
+    public string name{get; set;}
+    public decimal price{get; private set;} 
 
     public VendingItem(string givenName, decimal givenPrice)
     {
         this.name = givenName;
         this.price = Decimal.Round(givenPrice, 2); //rounds givenPrice to 2 decimal places
+    }
+
+    public void setPrice(decimal newPrice)
+    {
+        this.price = newPrice;
     }
 
 }

162580db6c52df8c2b3b8d0fe7d012915e1c42c7
bgrieme <bgrieme@gmail.com>
2019-11-25 03:01:55 -0600

    added method stringGUI() which returns a string representing the contents of the inventory in an user-friendly way.

--- src/Inventory.cs
+++ src/Inventory.cs
@@ -11,10 +11,10 @@ public class Inventory
             item = givenItem;
             quantity = givenQuantity;
         }
-    }    
+    }
     private slot[,] inv;
-    public int width {get; private set;}
-    public int height {get; private set;}
+    public int width { get; private set; }
+    public int height { get; private set; }
 
     public Inventory(int width, int height)
     {
@@ -46,11 +46,10 @@ public class Inventory
     Returns true if successfully inserted, false otherwise.*/
     public bool insertNew(VendingItem item, int quantity, int x, int y)
     {
-        if ((inv[x,y].item == default(VendingItem)))
+        if ((inv[x, y].item == default(VendingItem)))
         {
             this.inv[x, y] = new slot(item);
-            this.setQuantity
-    (quantity, x, y); //easy way of disallowing inserting a new item with a quantity of -x
+            this.setQuantity(quantity, x, y); //easy way of disallowing inserting a new item with a quantity of -x
             return true;
         }
         else
@@ -95,5 +94,65 @@ public class Inventory
             quantity = 0;
         }
         inv[x, y].quantity = quantity;
+    }
+
+    /*Produces a text-based graphical representation of the given inventory slot.
+    Really only intended for demo purposes.*/
+    public string stringGUI(int cellWidth = 12)
+    {
+        string s_inv = "";
+        for (int i = 0; i <= (cellWidth-1) * this.width; i++) //top border
+        {
+            s_inv += "-";
+        }
+        for (int h = this.height - 1; h >= 0; h--)
+        {
+            stringGUI_buildRow(ref s_inv, cellWidth, h, "NAME");
+            stringGUI_buildRow(ref s_inv, cellWidth, h, "QUANTITY");
+            stringGUI_buildRow(ref s_inv, cellWidth, h, "PRICE");
+            s_inv += "\n|";
+            for (int w = 0; w < this.width; w++) //slot grid location
+            {
+                string location = "[" + w + ", " + h + "]";
+                location = location.PadLeft((cellWidth-2-location.Length)/2 + location.Length).PadRight(cellWidth-2); //this beautifully center-aligns the location string within the cell. Formula is basically: (availableSpace - locationStringLength) / 2 + locationStringLength, then simply padRight(availableSpace)
+                s_inv += location + "|";
+            }
+            s_inv += "\n";
+            for (int i = 0; i <= (cellWidth-1) * this.width; i++) //bottom border
+            {
+                s_inv += "-";
+            }
+        }
+        return s_inv;
+    }
+
+    private void stringGUI_buildRow(ref string s_inv, int cellWidth, int h, string dataDesignation)
+    {
+        string validationString = "NAME QUANTITY PRICE";
+        if ( ! validationString.Contains(dataDesignation))
+        {
+            throw new System.Exception("Must provide valid dataDesignator value: \"NAME\", \"QUANTITY\", or \"PRICE\" are valid.");
+        }
+        s_inv += "\n|";
+        for (int w = 0; w < this.width; w++) //product names
+        {
+            string dataString = "";
+            if (this.inv[w, h].item == null){} //prevents any of the 'else if's from occurring if the target slot is empty.
+            else if (dataDesignation == "NAME")
+            {
+                dataString = this.inv[w, h].item.name;
+            }
+            else if (dataDesignation == "QUANTITY")
+            {
+                dataString = "Count: " + this.inv[w, h].quantity;
+            }
+            else if (dataDesignation == "PRICE")
+            {
+                dataString = "$" + this.inv[w, h].item.price;
+            }
+            dataString = (dataString.Length <= cellWidth - 2) ? dataString : dataString.Substring(0, cellWidth - 2); //trim the item name to fit within the cell, if needed
+            dataString = dataString.PadRight(cellWidth - 2);
+            s_inv += dataString + "|";
+        }
     }
 }

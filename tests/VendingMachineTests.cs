bbb00e34627233b3d63de085b8ab1a0fec91acd1
bgrieme <bgrieme@gmail.com>
2019-12-04 00:54:16 -0600

    Fixed incorrect expected value in getHoldingsString() test. Removed static modifier on default Inventory and Till objects. Moved construction of VendingMachine to be inside each test case.

--- tests/VendingMachineTests.cs
+++ tests/VendingMachineTests.cs
@@ -8,18 +8,17 @@ public class VendingMachineTests
     static Slot item3 = new Slot(new VendingItem("item3", .75m), 2);
     static Slot item4 = new Slot(new VendingItem("item4", 1m), 2);
     static Slot[] items = {item1, item2, item3, item4};
-    static Inventory inv_2x2 = new Inventory(2, 2, items);
-    static Till till_300 = new Till(300, 60, 30, 12, 3);
-    VendingMachine machine = new VendingMachine(inv_2x2, till_300);
-
+    Inventory inv_2x2 = new Inventory(2, 2, items);
+    Till till_300 = new Till(300, 60, 30, 12, 3);
 
     [Theory (DisplayName = "Inserting currency should change the Holding String")]
     [InlineData ("$0.00")]
     [InlineData ("$1.00", 50, 3, 1, 1)]
     [InlineData ("$1.99", 4, 0, 2, 3, 1)]
-    [InlineData ("$1000.00", 100, 0, 0, 0, 999)]
+    [InlineData ("$1,000.00", 100, 0, 0, 0, 999)]
     public void insertCurrency_should_change_holdingsValue(string expectedVal, int pennies=0, int nickels=0, int dimes=0, int quarters=0, int dollars=0)
     {
+        VendingMachine machine = new VendingMachine(inv_2x2, till_300);
         machine.insertCurrency(Currency.PENNY, pennies);
         machine.insertCurrency(Currency.NICKEL, nickels);
         machine.insertCurrency(Currency.DIME, dimes);
@@ -31,6 +30,7 @@ public class VendingMachineTests
     [Fact (DisplayName = "Attempt to vend two items with all checks met.")]
     public void vendItem_getDispensedItem_vendAnotherItem_allChecksMet()
     {
+        VendingMachine machine = new VendingMachine(inv_2x2, till_300);
         machine.insertCurrency(Currency.PENNY, 2);
         //first vending
         Assert.True(machine.vend(0, 1), "Vending first item failed.");
@@ -47,6 +47,7 @@ public class VendingMachineTests
     [Fact (DisplayName = "Attempt to vend item without adequate holdings. Should not vend! Then add holdings, should vend.")]
     public void attemptToVend_inadequateHoldings_shouldNotVend_addHoldings_shouldNowVend()
     {
+        VendingMachine machine = new VendingMachine(inv_2x2, till_300);
         machine.insertCurrency(Currency.DIME, 9); //90 cents in holdings
         Assert.False(machine.vend(1,0), "Item should not have vended, but it did.");
         machine.insertCurrency(Currency.DIME, 1); //100 cents in holdings
@@ -57,7 +58,7 @@ public class VendingMachineTests
     public void attemptToVend_changeCannotBeMade_vendFails_thenAddProperChange_vendSucceeds()
     {
         Till till = new Till(0, 0, 0, 2, 1); //two quarters, one dollar.
-        machine = new VendingMachine(inv_2x2, till); //reconstruct vendingmachine with new till
+        VendingMachine machine = new VendingMachine(inv_2x2, till); //reconstruct vendingmachine with new till
         machine.insertCurrency(Currency.DOLLAR, 1); //insert $1
         Assert.False(machine.vend(0,0), "Vending should have failed due to inability to return exact change, but vend() returned true anyway.");
         machine.insertCurrency(Currency.QUARTER, 1); //machine now holds 3 quarters and two dollars. Exact change can be made
